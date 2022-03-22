8a9d3958fd3caf87d37d78a7e196fd3e708f44c5
Vibin Varghese <mailme.vibin@gmail.com>
2022-03-22 10:55:09 +0000

    Lab B02 DonationX-v2 - Adding Actions to Navigate

---
+++ app/src/main/java/ie/wit/donationx/fragments/ReportFragment.kt
@@ -0,0 +1,71 @@
+package ie.wit.donationx.fragments
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.recyclerview.widget.LinearLayoutManager
+import ie.wit.donationx.R
+import ie.wit.donationx.adapters.DonationAdapter
+import ie.wit.donationx.databinding.FragmentReportBinding
+import ie.wit.donationx.main.DonationXApp
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ReportFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ReportFragment : Fragment() {
+
+    lateinit var app: DonationXApp
+    private var _fragBinding: FragmentReportBinding? = null
+    private val fragBinding get() = _fragBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        app = activity?.application as DonationXApp
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _fragBinding = FragmentReportBinding.inflate(inflater, container, false)
+        val root = fragBinding.root
+        activity?.title = getString(R.string.action_report)
+
+        fragBinding.recyclerView.setLayoutManager(LinearLayoutManager(activity))
+        fragBinding.recyclerView.adapter = DonationAdapter(app.donationsStore.findAll())
+
+        return root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ReportFragment().apply {
+                arguments = Bundle().apply { }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _fragBinding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_report, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,
+            requireView().findNavController()) || super.onOptionsItemSelected(item)
+    }
+}
